eff00be953977264eb69f57edac227378f75eeb9
yanliu <ryuyan@gmail.com>
2018-12-17 21:16:35 -0800

    add partition

--- src/main/scala/SparkTest.scala
+++ src/main/scala/SparkTest.scala
@@ -1,4 +1,6 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.{row_number,collect_list}
+import org.apache.spark.sql.expressions.Window
 
 object SparkWordCount {
   def main(args: Array[String]) {
@@ -11,7 +13,15 @@ object SparkWordCount {
     val catdf = spark.read.json("src/main/resources/category.json")
     // Creates a temporary view using the DataFrame
     catdf.createOrReplaceTempView("category")
-    val resultDF = spark.sql("SELECT * FROM example e join category c on e.catId=c.catId ")
+    val resultDF = spark.sql("SELECT e.itemId, e.catId, e.transDate  FROM example e join category c on e.catId=c.catId ")
     resultDF.show()
+    val w = Window.partitionBy($"itemId").orderBy($"transDate".desc)
+    val dfTop = resultDF.withColumn("rn", row_number.over(w)).where($"rn" <=2).drop("rn")
+    dfTop.show;
+
+    dfTop.groupBy("itemId")
+      .agg(collect_list("catId").alias("catIds"))
+      .write.json("src/main/resources/output")
+
   }
 }
